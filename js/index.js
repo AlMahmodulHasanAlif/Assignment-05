4818fe0c9f260f7f3441cc1de6dd0ce1e6f3d398
Al Mahmodul Hasan Alif <almahmodulhasanalif@gmail.com>
2025-08-30 11:52:06 +0600

    call history time added

--- js/index.js
+++ js/index.js
@@ -27,13 +27,36 @@ document
       totalStarPoints = starPoints - 20;
       document.getElementById("star-points").innerText = totalStarPoints;
     }
+    //alert calling
     const serviceName =
       e.target.parentNode.parentNode.children[1].children[1].innerText;
-    console.log(serviceName);
     const serviceNumber =
       e.target.parentNode.parentNode.children[1].children[3].innerText;
-    console.log(serviceNumber);
-    // let serviceNumber = "d";
-
     alert("Calling" + " " + serviceName + " " + serviceNumber + "...");
+    //call history
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    const historyContainer = document.getElementById("history-container");
+    const callHistory = document.createElement("div");
+    callHistory.innerHTML = `
+            <div class="flex justify-between items-center bg-[#FAFAFA] p-2 rounded mt-2">
+              <div>
+                <p class="font-semibold text-[.8rem]">
+                  ${serviceName}
+                </p>
+                <p class="text-[.7rem] text-[#5C5C5C]">999</p>
+              </div>
+              <div class="text-[.8rem] hind-madurai-regular whitespace-nowrap">
+                ${hours}:${minutes}:${seconds}:${ampm}
+              </div>
+            </div>
+    `;
+    historyContainer.append(callHistory);
   });
