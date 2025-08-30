b558c1b054e2a8160c07146010bb436e1a69a76f
Al Mahmodul Hasan Alif <almahmodulhasanalif@gmail.com>
2025-08-30 10:21:04 +0600

    heart increse funtionality added

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const heartIcon = document.querySelectorAll(".heart-icon");
-
+//heart increase
 document
   .getElementById("card-container")
   .addEventListener("click", function (e) {
@@ -9,4 +9,31 @@ document
       heartPointsIncrease++;
       document.getElementById("heart-points").innerText = heartPointsIncrease;
     }
+  });
+//call button
+document
+  .getElementById("card-container")
+  .addEventListener("click", function (e) {
+    if (e.target.className.includes("call-btn")) {
+      const starPoints = Number(
+        document.getElementById("star-points").innerText
+      );
+      if (starPoints < 20) {
+        alert(
+          "❌ You do not have enough coins.Minimum 20coins needed to make a call."
+        );
+        return;
+      }
+      totalStarPoints = starPoints - 20;
+      document.getElementById("star-points").innerText = totalStarPoints;
+    }
+    const serviceName =
+      e.target.parentNode.parentNode.children[1].children[1].innerText;
+    console.log(serviceName);
+    const serviceNumber =
+      e.target.parentNode.parentNode.children[1].children[3].innerText;
+    console.log(serviceNumber);
+    // let serviceNumber = "d";
+
+    alert("Calling" + " " + serviceName + " " + serviceNumber + "...");
   });
